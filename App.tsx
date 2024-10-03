9bfce267236a5727be005d90be0a8d64a4e8be94
aswath06 <maswath55@gmail.com>
2024-10-03 09:27:44 +0530

    Aswath third

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Fullstart from './src/screens/login/Fullstart'
-import Login from './src/screens/drivers/My_job/My_Job_Details'
+import Login from './src/screens/login/login'
+import Sigin from './src/screens/login/signin'
+import PaymentCompleted from './src/screens/drivers/Qr_Code.js/Payment_Success'
+import Vehicle from './src/screens/Admin/Vehicle_Details'
+import Getstarted from './src/screens/login/get-started'
+import Jobdetails from './src/screens/drivers/My_job/My_Job_Details'
 export default function App() {
   return (
-      <Login/>
+    <Sigin/>
   )
 }
 
